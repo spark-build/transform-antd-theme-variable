1ba11c965062a35b3325c6b64e0a9c484ad3085e
atzcl <atzcl0310@gmail.com>
2021-07-16 14:35:01 +0800

    refactor: remove less function

--- src/generateThemeVariable/getThemeVariable.ts
+++ src/generateThemeVariable/getThemeVariable.ts
@@ -1,13 +1,26 @@
 import { generate } from '@ant-design/colors';
-// @ts-ignore
-import colorFunctions from 'less/lib/less/functions/color';
-// @ts-ignore
-import Color from 'less/lib/less/tree/color';
+import { TinyColor } from '@ctrl/tinycolor';
+import { clamp01 } from '@ctrl/tinycolor/dist/util';
 // import { logAllReplaceVariables } from './logAllReplaceVariables';
 
 interface Params {
   primaryColor: string;
 }
+
+const lighten = (color: string, amount?: number): string =>
+  new TinyColor(color).lighten(amount).toString();
+
+const darken = (color: string, amount?: number): string =>
+  new TinyColor(color).darken(amount).toString();
+
+// https://github.com/less/less.js/blob/b37922cfb2932f00d1e8b340f4799ff24a2af0f2/packages/less/src/less/functions/color.js#L318
+const fade = (color: string, amount: number = 10): string => {
+  const hsl = new TinyColor(color).toHsl();
+  hsl.a = amount / 100;
+  hsl.a = clamp01(hsl.a);
+
+  return new TinyColor(hsl).toString();
+};
 
 export const getThemeVariable = ({ primaryColor }: Params) => {
   if (
@@ -17,12 +30,10 @@ export const getThemeVariable = ({ primaryColor }: Params) => {
     throw new Error('[generateThemeVariable] primaryColor 不是一个合法的颜色值');
   }
 
-  const primaryColorNode = new Color(primaryColor.slice(1), undefined, primaryColor);
-
   const primaryColorLightenColors = [20, 35, 50, 70].reduce(
     (prev, value) => ({
       ...prev,
-      [value]: colorFunctions.lighten(primaryColorNode, { value }).toCSS(),
+      [value]: lighten(primaryColor, value),
     }),
     {} as Record<string, string>,
   );
@@ -32,19 +43,17 @@ export const getThemeVariable = ({ primaryColor }: Params) => {
 
   const variable = {
     '--primary-color': primaryColor,
-    '--primary-color-fade': colorFunctions.fade(primaryColorNode, { value: 4 }).toCSS(),
+    '--primary-color-fade': fade(primaryColor, 4),
     '--primary-active-color': primaryColorLightenColors[70],
     '--primary-hover-color': primaryColorLightenColors[50],
     '--primary-color-lighten-20': primaryColorLightenColors[20],
     '--primary-color-lighten-35': primaryColorLightenColors[35],
     '--link-hover-color': colors[4],
     '--link-active-color': colors[6],
-    '--table-selected-row-bg--darken-2': colorFunctions
-      .darken(primaryColorNode, { value: 35 })
-      .toCSS(),
-    '--primary-color--lighten-20': colorFunctions.lighten(primaryColorNode, { value: 20 }).toCSS(),
-    '--primary-color--lighten-35': colorFunctions.lighten(primaryColorNode, { value: 35 }).toCSS(),
-    '--primary-color--fade-12': colorFunctions.fade(primaryColorNode, { value: 12 }).toCSS(),
+    '--table-selected-row-bg--darken-2': darken(primaryColor, 35),
+    '--primary-color--lighten-20': lighten(primaryColor, 20),
+    '--primary-color--lighten-35': lighten(primaryColor, 35),
+    '--primary-color--fade-12': fade(primaryColor, 12),
     // '--primary-color--tint-20': colorFunctions.tint(primaryColor, '20%'),
     // '--primary-color--tint-50': colorFunctions.tint(primaryColor, '50%'),
   } as Record<string, string>;
